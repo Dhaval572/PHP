f6e402c4b417bcef10db2161b33a31d8c65d523c
Dhaval572 <dp9718776@gmail.com>
2025-01-02 16:39:32 +0530

    Create For-each_loop.php

---
+++ For-each_loop.php
@@ -0,0 +1,14 @@
+<?php
+
+# for-each loop:
+
+	$arr = array("God of war", "Hitman", "Assassin creed", "Ghost of tsushima", "Tomb raider");
+
+	foreach($arr as $value)
+	{
+		print($value . "<br>");
+	}
+
+	// This loop will take each value from the array's elements and assign it to the variable value one by one.
+
+?>
