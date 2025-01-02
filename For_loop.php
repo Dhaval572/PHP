99af558332e97d212627bd72a7ef305a0f31341c
Dhaval572 <dp9718776@gmail.com>
2025-01-02 16:19:10 +0530

    Create For_loop.php

---
+++ For_loop.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+For loop:
+
+	Syntax:
+
+		for(initialization; condition; increment/decrement )
+		{
+			// Statements
+		}
+
+*/
+
+// Example:
+
+	for ($i = 1; $i <= 10; $i++) 
+	{
+		print($i."<br>");
+	}
+
+?>
