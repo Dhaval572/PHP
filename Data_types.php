c7b1dfdecf088e6feb37a67e7c353b9444d7a463
Dhaval572 <dp9718776@gmail.com>
2025-01-02 12:00:28 +0530

    Create Data_types.php

---
+++ Data_types.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+
+PHP Data Types:
+
+	1. String   : A sequence of characters, typically enclosed in quotes
+	2. Integer	: A whole number without a decimal point
+	3. Float	: A number that has a decimal point
+	4. boolean	: A value representing true or false
+	5. Object	: An instance of a class containing properties and methods.
+	6. Array	: A collection of values, which can be of any type, indexed by keys.
+	7. NULL		: Represents a variable with no value or an undefined value.
+
+*/
+
+// Example of each:
+
+	$name = "XYZ"; 		// String
+	$radius = "45.45"; 	// FLoat
+	$age = "34"; 		// Integer
+	$isTrue = true; 	// Boolean
+	$arr = [1, 2, 3]; 	// Array
+	$var = NULL; 		//  NULL
+
+	// Print the array
+
+	// print var_dump($arr); // var_dump is used to print array's elements
+
+	// You can also print element by index
+
+	print($arr[0]); // Printing first
+	echo "<br>"; 	// For new line
+
+	print($arr[1]);	// Printing second
+	echo "<br>";	// For new line
+
+	print($arr[2]);	// Printing third
+	echo "<br>";	// For new line\
+
+	// ( Note: you can print all elements by loops also but this is only data type explainaton so ... )
+
+?>
