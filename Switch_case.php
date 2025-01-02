14456bf935f2cb4811570bd41385e9c0351492a1
Dhaval572 <dp9718776@gmail.com>
2025-01-02 15:56:34 +0530

    Create Switch_case.php

---
+++ Switch_case.php
@@ -0,0 +1,30 @@
+<?php
+
+# switch case:
+
+$language = "C/C++";
+
+switch($language)
+{
+    case "C/C++":
+        print("You are real and very strong programmer");
+        break;
+        
+    case "Java":
+        print("The possibility of you falling into depression is higher.");
+        break;
+        
+    case "Python":
+        print("You can't do high performance tasks");
+        break;
+        
+    case "Java script":
+        print("Chances of going insane are high");
+        break;
+        
+    default:
+        print("Prepare yourself");
+        break;
+}
+
+?>
