f71075403ed7a55eeb59b7284a39c6db7685962f
Dhaval572 <dp9718776@gmail.com>
2025-01-02 11:41:27 +0530

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+Rules for creating variable in php
+
+1.	Start with $ sign
+
+2. 	Cannot start with numbers/number: 
+
+3. 	Must start with a latter or an underscore
+
+4. 	Can only contain alphanumeric characters and underscores
+
+5.	variables are case sensitive in php
+
+*/
+
+// Example 
+
+$dialog = "Me jukega nahi sala";
+
+print("Dialog of pushpa is: $dialog");
+
+?>
