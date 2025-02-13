4229282fb2b787646921f4590db00a3719f81e5d
Dhaval <dp9718776@gmail.com>
2025-02-13 09:53:30 +0530

    Create

--- Date-time_functions.php
+++ Date-time_functions.php
@@ -1,7 +1,21 @@
 <?php
 
 // Date / Time function in php
-print_r(getdate()); // Returns an array with the current date and time details.
+print_r(getdate()) ; echo "<br>"; // Returns an array with the current date and time details.
 
+$date1 = date_create("2025-02-13");
+$date2 = date_create("2024-02-13");
+$diff = date_diff($date1, $date2);
+echo $diff->format("%y years, %m months, %d days") . "<br>";
+
+// checkdate() - Checks if a given date is valid.
+echo checkdate(2, 29, 2024) ? "Valid" : "Invalid";
+echo "<br>";
+
+// Returns the current Unix timestamp	
+echo time() . "<br>";
+
+// Returns the Unix timestamp for a given date and time.
+echo mktime(14, 30, 0, 2, 13, 2025) . "<br>";
 
 ?>
