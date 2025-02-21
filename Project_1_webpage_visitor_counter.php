bef9d8e8a8c41a30e5911134ccede833c618d816
Dhaval <dp9718776@gmail.com>
2025-02-21 19:10:55 +0530

    Update

--- Project_1_webpage_visitor_counter.php
+++ Project_1_webpage_visitor_counter.php
@@ -9,9 +9,13 @@ if (!file_exists($filename)) {
     file_put_contents($filename, 0);
 }
 
-// Read, increment, and update counter
 $visits = file_get_contents($filename);
 $visits++;
 file_put_contents($filename, $visits);
 
 echo "This page has been visited $visits times.";
+
+if($visits == 999)
+{
+    echo "<h1><b>My webpage got 999 viewers!";
+}
